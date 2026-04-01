5d04c7b36ab4be39228d3ac773644feff022bfd0
Luis Montes <monteslu@gmail.com>
2026-03-31 19:51:25 -0700

    wc_log: also write to file on Android (logcat buffer too small)

--- include/wc_log.h
+++ include/wc_log.h
@@ -8,15 +8,26 @@
 #include <android/log.h>
 #define WC_LOG_TAG "wasmcart"
 
+static FILE* _wc_log_file = NULL;
+
+static inline void _wc_log_ensure_file(void) {
+    if (!_wc_log_file) {
+        _wc_log_file = fopen("/storage/emulated/0/racarts/wasmcart.log", "w");
+    }
+}
+
 static inline void wc_log(const char* fmt, ...) {
     va_list args;
     va_start(args, fmt);
     __android_log_vprint(ANDROID_LOG_INFO, WC_LOG_TAG, fmt, args);
     va_end(args);
-    // Also write to stderr for non-Android hosts that capture it
-    va_start(args, fmt);
-    vfprintf(stderr, fmt, args);
-    va_end(args);
+    _wc_log_ensure_file();
+    if (_wc_log_file) {
+        va_start(args, fmt);
+        vfprintf(_wc_log_file, fmt, args);
+        va_end(args);
+        fflush(_wc_log_file);
+    }
 }
 
 static inline void wc_log_err(const char* fmt, ...) {
@@ -24,9 +35,13 @@ static inline void wc_log_err(const char* fmt, ...) {
     va_start(args, fmt);
     __android_log_vprint(ANDROID_LOG_ERROR, WC_LOG_TAG, fmt, args);
     va_end(args);
-    va_start(args, fmt);
-    vfprintf(stderr, fmt, args);
-    va_end(args);
+    _wc_log_ensure_file();
+    if (_wc_log_file) {
+        va_start(args, fmt);
+        vfprintf(_wc_log_file, fmt, args);
+        va_end(args);
+        fflush(_wc_log_file);
+    }
 }
 
 #else
