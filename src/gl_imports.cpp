27450f92545807430efc72548ec7d4257b0d4afe
Luis Montes <monteslu@gmail.com>
2026-04-02 19:23:09 -0700

    fix missing paren

--- src/gl_imports.cpp
+++ src/gl_imports.cpp
@@ -328,7 +328,7 @@ GL_REG(glStencilFuncSeparate, 4, 0, glStencilFuncSeparate(A_U32(0), A_U32(1), A_
 GL_REG(glStencilOp, 3, 0, glStencilOp(A_U32(0), A_U32(1), A_U32(2)))
 GL_REG(glStencilOpSeparate, 4, 0, glStencilOpSeparate(A_U32(0), A_U32(1), A_U32(2), A_U32(3)))
 GL_REG(glStencilMask, 1, 0, glStencilMask(A_U32(0)))
-GL_REG(glStencilMaskSeparate, 2, 0, glStencilMaskSeparate(A_U32(0), A_U32(1))
+GL_REG(glStencilMaskSeparate, 2, 0, glStencilMaskSeparate(A_U32(0), A_U32(1)))
 })
 
 // ─── Face culling ─────────────────────────────────────────────────────────
